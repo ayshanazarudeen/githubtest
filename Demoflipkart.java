ec674d732f539c1ecd9a019f1132d15d872e8f94
Binsamol <92846100+Binsamol@users.noreply.github.com>
2021-10-21 10:13:19 +0530

    Update Demoflipkart.java

--- Demoflipkart.java
+++ Demoflipkart.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Demoflipkart {
+public class Demoflipkert {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","D://Software//SEL_JAR-20210901T092836Z-001//SEL_JAR//chromedriver_win32//chromedriver.exe");
